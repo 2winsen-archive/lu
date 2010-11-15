ee997d146c761c1358cc71060c6095cc23b1035f
vitalijs.shakels <vitalijs.shakels@4656af93-5b05-c73c-0ad6-876561d334af>
2010-11-15 00:31:05 +0000

    Fixed empty areas

--- OOP_U529/src/lu/lv/chart/PieChart.java
+++ OOP_U529/src/lu/lv/chart/PieChart.java
@@ -19,22 +19,27 @@ public class PieChart implements Chart {
 
 		// Draw each pie slice
 		double curValue = 0.0D;
-		int startAngle = 0;
+		int checkAngle = 0;
 		for (ChartItem chartItem : data) {
-			// Compute the start and stop angles
-			startAngle = (int) Math.round(curValue * 360 / total);
 			int arcAngle = (int) Math.round(chartItem.getValue() * 360 / total);
 
 			// Ensure that rounding errors do not leave a gap between the first and last slice
 			if (chartItem.hashCode() == data.get(data.size() - 1).hashCode()) {
-				arcAngle = 360 - startAngle;
+				arcAngle = 360 - checkAngle;
 			}
 
 			// Set the color and draw a filled arc
 			g.setColor(chartItem.getColor());
-			g.fillArc(0, 0, canvas.getWidth(), canvas.getHeight(), startAngle, arcAngle);
+			
+			System.out.println(checkAngle);
+			System.out.println(arcAngle);
+			System.out.println();
+			
+			
+			g.fillArc(0, 0, canvas.getWidth(), canvas.getHeight(), checkAngle, arcAngle);
 
 			curValue += chartItem.getValue();
+			checkAngle = checkAngle + arcAngle;
 		}
 	}
 
